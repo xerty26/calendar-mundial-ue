e8c5e5026c5c91deb69d178eba2828c728dd1a2a
sergio.gonzalez <sergio.gonzalez@unidadeditorial.es>
2026-06-10 11:45:29 +0200

    pp

--- cron-update.js
+++ cron-update.js
@@ -110,7 +110,8 @@ async function actualizarCalendarioFisico() {
 
         // 3. Inyección de los partidos de la API
         partidosNormalizados.forEach(partido => {
-            const summary = `${partido.homeTeamFlag} ${partido.homeTeamName} vs ${partido.awayTeamFlag} ${partido.awayTeamName} | Marca Mundial 2026`;
+            //const summary = `${partido.homeTeamFlag} ${partido.homeTeamName} vs ${partido.awayTeamFlag} ${partido.awayTeamName} | Marca Mundial 2026`;
+            const summary = `${partido.homeTeamName} vs ${partido.awayTeamName} | Marca Mundial 2026`;
             icsContenido += [
                 "BEGIN:VEVENT",
                 `UID:${partido.id}@marca.com`,
